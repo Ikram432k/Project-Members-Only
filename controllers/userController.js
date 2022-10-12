2aa633e2600764082e41bf7d71441ad6f0dfaf2a
ikram mohideen <ikram432k@gmail.com>
2022-10-12 19:44:27 +0530

    user controller created

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/user');
+
+exports.member_get =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.member_post =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.admin_get =(req,res)=>{
+    res.send("not implemented");
+}
+
+exports.admin_post =(req,res)=>{
+    res.send("not implemented");
+}
