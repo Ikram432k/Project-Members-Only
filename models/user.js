44f44456b3d5936e5bc58efdfa8f7754093789c6
ikram mohideen <ikram432k@gmail.com>
2022-10-13 20:18:54 +0530

    created user model for database

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     username:{type:String ,required:true ,maxLength:20},
     password:{type:String, required:true},
     member:{type:Boolean, default:false},
-    admim:{type:Boolean, default:false},
+    admin:{type:Boolean, default:false},
 });
 
 module.exports = mongoose.model("User",userSchema);
