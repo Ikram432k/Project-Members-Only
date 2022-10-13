b99a4b3a8c8757f0fcaf00959f15359650cd9737
ikram mohideen <ikram432k@gmail.com>
2022-10-13 20:16:11 +0530

    added controller for creating and deleting message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require("express-validator");
 const Message = require('../models/message');
 
 exports.index = async (req, res, next) => {
@@ -10,13 +11,34 @@ exports.index = async (req, res, next) => {
   }
 };
 exports.create_message_get =(req,res)=>{
-    res.send("not implemented");
+  if (!res.locals.currentUser) return res.redirect("/"); 
+  res.render("newMessage",{user:res.locals.currentUser});
 }
 
-exports.create_message_post =(req,res)=>{
-    res.send("not implemented");
-}
+exports.create_message_post =[
+  body("title").trim().isLength({min:1}).escape().withMessage("Title for your message is must"),
+  body("text").trim().isLength({min:1}).escape().withMessage("Message must not be empty"),
+  (req,res,next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty){
+      return res.render("newMessage",{errors:errors});
+    }
+    const message = new Message({
+      user:req.user.id,
+      title:req.body.title,
+      text:req.body.text,
+      timeStamp:Date.now()
+    });
+    message.save((err)=>{
+      if(err) return next(err);
+      res.redirect("/");
+    });
+  }
+]
 
-exports.delete_message_post =(req,res)=>{
-    res.send("not implemented")
+exports.delete_message_post =(req,res,next)=>{
+  Message.findByIdAndRemove(req.body.messageId,(err)=> {
+    if (err) return next(err);
+    res.redirect("/");
+  });
 }
