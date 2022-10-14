2425ce2cd3f6b5cb41dfeb599ea08e54f14236fa
ikram mohideen <ikram432k@gmail.com>
2022-10-14 18:45:55 +0530

    compression added

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user");
 const bcrypt = require("bcryptjs");
 const session = require("express-session"); // Dependency of passport.js
+const compression = require("compression");
+const helmet = require("helmet");
 
 // Establish database connection
 const mongoose = require("mongoose");
@@ -24,11 +26,13 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(compression()); // Compress all routes
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 passport.use(new LocalStrategy((username, password, done) => {
